cbddb9b289eb41d27ef9b43105239c659c3b6670
MrFatra <ramadhanfatra2004@gmail.com>
2026-05-05 23:19:28 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="text-5xl font-bold text-primary font-grotesk">Starting here, mates!</div>
+      </section>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00D1FF',
+        secondary: '#FF6B00',
+        tertiary: '#A0AEC0',
+        neutral: '#0F1115',
+      },
+      fontFamily: {
+        grotesk: ['Space Grotesk', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      }
+    }
+  },
+  plugins: [],
+}
